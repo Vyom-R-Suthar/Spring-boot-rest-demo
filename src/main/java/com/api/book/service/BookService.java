19e8d1164506a06f545963a9c8c97228efd27217
Vyom-R-Suthar <vyomrsuthar99@gmail.com>
2024-03-18 14:34:23 +0530

    Created BookRepository and modified BookService

--- src/main/java/com/api/book/service/BookService.java
+++ src/main/java/com/api/book/service/BookService.java
@@ -1,53 +1,44 @@
 // Jai Swaminarayan KESHAV , Swami-Shreeji
 package com.api.book.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.book.dao.BookRepository;
 import com.api.book.entities.Book;
 
 @Service
 public class BookService {
 	
-	private static List<Book> list = new ArrayList<>();
-	
-	static {		
-		list.add(new Book(1,"Java in depth","Dheeru Munduluru"));
-		list.add(new Book(2,"Head first Java","ABC"));
-		list.add(new Book(3,"Thinks in Java","XYZ"));
-	}
+	@Autowired
+	private BookRepository repository;
 	
 	public List<Book> getBooks(){
-		return list;
+		List<Book> books = repository.findAll();
+		return books;
 	}
 	
 	public Book getBookById(int id) {
-		Book book = null;
-		try {
-		book = list.stream().filter(e -> e.getId() == id).findFirst().get();
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+		Book book = repository.findById(id);
 		return book;
 	}
 	
 	public Book addBook(Book book) throws Exception {
-		list.add(book);
-		return book;
+		Book addedBook = repository.save(book);
+		return addedBook;
 	}
 	
 	public Book deleteBook(int id) {
-		Book book = getBookById(id);
-		boolean remove = list.remove(book);
-		return (remove == true? book:null);
+		Book book = repository.findById(id);
+		repository.deleteById(id);
+		return book;
 	}
 
 	public Book updateBook(int id, Book book) {
-		Book bookToUpdate = getBookById(id);
-		bookToUpdate.setTitle(book.getTitle());
-		bookToUpdate.setAuthor(book.getAuthor());
-		return bookToUpdate;
+		book.setId(id);
+		Book updatedBook = repository.save(book);
+		return updatedBook;
 	}
 }
