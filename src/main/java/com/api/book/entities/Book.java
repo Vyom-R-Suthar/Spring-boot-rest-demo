e074ef32b8ae6d1a5e93554cd7d9cf5110413005
Vyom-R-Suthar <vyomrsuthar99@gmail.com>
2024-03-18 14:08:18 +0530

    added database properties in application.properties file

--- src/main/java/com/api/book/entities/Book.java
+++ src/main/java/com/api/book/entities/Book.java
@@ -1,11 +1,22 @@
 // Jai Swaminarayan KESHAV , Swami-Shreeji
 package com.api.book.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="books")
 public class Book {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String title;
 	private String author;
+	
 	public int getId() {
 		return id;
 	}
