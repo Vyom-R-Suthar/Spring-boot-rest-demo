314e994b8a15295da2a9bca768ed05727c68d0f8
Vyom-R-Suthar <vyomrsuthar99@gmail.com>
2024-03-19 17:46:13 +0530

    Updated front end

--- src/main/java/com/api/book/BootrestbookApplication.java
+++ src/main/java/com/api/book/BootrestbookApplication.java
@@ -4,6 +4,7 @@ package com.api.book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BootrestbookApplication {
 
